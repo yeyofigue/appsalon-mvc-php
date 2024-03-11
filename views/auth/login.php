cc8ae81457080313bef899c9d4d8c333f88623f8
Sergio Figueroa <yeyofigue1986@hotmail.com>
2024-03-11 10:39:07 -0500

    first commit

---
+++ views/auth/login.php
@@ -0,0 +1,23 @@
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<?php 
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" type="email" id="email" placeholder="Tu Email" name="email">
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu Password" name="password">
+    </div>
+    <input type="submit" class="boton" value="Iniciar Sesion">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Aún no tienes una cuenta? Crea una</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+</div>
